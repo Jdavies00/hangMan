a102c1706aed7b2ea70dda54b61b37ea7824acfd
James Davies <j.davies2099@gmail.com>
2021-03-16 16:37:41 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void PrintMessage(string message, bool printTop= true, bool printBottom = true)
+
+{
+    if (printTop)
+    {
+        cout << "+---------------------------------+" << endl;
+        cout << "|";
+    }
+    bool front = true ; 
+    for (int i = message.length(); i < 33;  i++)
+    {}
+}
+int main()
+{
+
+getchar();
+return 0;
+
+}
